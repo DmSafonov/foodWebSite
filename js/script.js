957155923796ed0be029eaf9abcc9642d9a332fc
DmSafonov <safonov-dmitry@list.ru>
2023-01-24 18:42:56 +0400

    npxServ

--- js/script.js
+++ js/script.js
@@ -294,6 +294,10 @@ window.addEventListener('DOMContentLoaded', function () {
             hideModal();
         }, 4000)
     };
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res))
 });
 
 
